ccbab53850a6d1136d7ae169e415db36254343e2
Anthony Miyoro <amiyoro@gmail.com>
2019-06-02 22:46:43 +0300

    Update submission.php

--- submission.php
+++ submission.php
@@ -1,4 +1,5 @@
 <!DOCTYPE html>
+<html>
     <head>
         <title>API RAT 1</title>
     </head>
@@ -19,35 +20,21 @@
                 array("company"=>"Narnia Gmb.", "Amounts"=>"530,-23,670,3,-1299" , "Debits"=>$narnia_debits, "Balance"=>$narnia_gmb),
                 array("company"=>"Loneislands Pty", "Amounts"=>"-7568,39,49,-37,3000,98" , "Debits"=>$loneislands_debits, "Balance"=>$loneislands_pty));
 
-                if (count($companies) > 0){
-                    <table>
-                    <thead>
-                        <tr>
-                        <th><?php echo implode('</th><th>', array_keys(current($companies))); ?></th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                    foreach ($companies as $row): array_map('htmlentities', $row);
-                        <tr>
-                        <td><?php echo implode('</td><td>', $row); ?></td>
-                        </tr>
-                    endforeach; =
-                    </tbody>
-                    </table>
-                     endif;
 
-                }
-               
-                
+                if (count($companies) > 0): 
+                    echo "<table> <thead> <tr> <th>";
+                    echo implode('</th><th>', array_keys(current($companies)));
+                    echo "</th> </tr> </thead> <tbody>";
+                    foreach ($companies as $row): array_map('htmlentities', $row); 
+                        echo"<tr> <td>"; 
+                        echo implode('</td><td>', $row); 
+                        echo "</td> </tr>";
+                    endforeach;
+                        echo "</tbody> </table>";
+                 endif; 
             }
          
             formatArray($archenland_balance,$narnia_gmb, $loneislands_pty, $archenland_debits, $narnia_debits,  $loneislands_debits);
         ?>
-
-                      
-       
-
     </body>
-</html>
-
-
+</html> 
