ac591afe3d0e61c39d7a8141d781acbdca99eb9f
Anthony Miyoro <amiyoro@gmail.com>
2019-11-04 19:59:06 +0300

    Defined endpoints and added expected fields

--- app/src/main/java/com/example/androidchatbot/WResponse.java
+++ app/src/main/java/com/example/androidchatbot/WResponse.java
@@ -3,6 +3,11 @@ package com.example.androidchatbot;
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.POST;
+import retrofit2.http.Query;
+
 //  This is what our response looks like:
 //        {
 //        "status": true,
@@ -33,5 +38,12 @@ public class WResponse {
 
     public void setResponse(String response) {
         this.response = response;
+    }
+
+    public interface BotAPIs{
+
+        /* Over here we are sending a POST request with two fields as POST request body params */
+        @POST("/")
+        Call < WResponse > postUserMessage(@Field("user-input") String userInput);
     }
 }
